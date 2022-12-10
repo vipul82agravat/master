802fe3b243e385326f5ab6821be76cb8a137175f
vipul82agravat <vipulagravat@aum.bz>
2022-12-10 14:21:26 +0530

    save changes

--- test.php
+++ test.php
@@ -1,1 +1,8 @@
-test
+Add a remote origin and Push:
+git remote add origin https://github.com/vipul82agravat/master.git
+
+git remote -v
+
+git push
+
+Now the git push command pushes the changes in your local repository up to the remote repository you specified as the origin
