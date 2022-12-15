cf60e79a95e2c894f923bcbeee149f83f980bdc9
vipul82agravat <vipulagravat@aum.bz>
2022-12-15 18:47:11 +0530

    my fifth commit for composer leran and make phpmailer lib with composer exmple

---
+++ PHP/Composer/sendmail/Mail.php
@@ -0,0 +1,47 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'vipulagravat@aum.bz';                     //SMTP username
+    $mail->Password   = 'Vipulc@a';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('vipulagravat@aum.bz', 'LibMailer');
+    $mail->addAddress('vipulagravat@aum.bz', 'vip');     //Add a recipient
+    //$mail->addAddress('ellen@example.com');               //Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'First composer test Mail';
+    $mail->Body    = 'First Mail send  <b>in bold!</b>';
+    $mail->AltBody = 'Mail Send';
+
+    $mail->send();
+    echo 'Eamil Message has been sent';
+} catch (Exception $e) {
+    echo "Email Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
