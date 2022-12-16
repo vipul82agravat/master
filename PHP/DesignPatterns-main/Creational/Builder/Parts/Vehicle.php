1e9df258af065e95ea99040c82883746884854f6
vipul82agravat <vipulagravat@aum.bz>
2022-12-16 19:40:16 +0530

    my six commit for php designPatternas and leran and docs and practical

---
+++ PHP/DesignPatterns-main/Creational/Builder/Parts/Vehicle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DesignPatterns\Creational\Builder\Parts;
+
+abstract class Vehicle
+{
+    final public function setPart(string $key, object $value)
+    {
+    }
+}
